21d747963eb27b457b1644dda83104088dd6b16c
Jaiditya <jaidityachopra@gmail.com>
2024-03-31 20:33:27 +0530

    Made change

--- Predictions/urls.py
+++ Predictions/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import include, url
+from django.conf.urls import include, re_path
 from django.contrib import admin
 
 urlpatterns = [
@@ -6,14 +6,14 @@ urlpatterns = [
     # url(r'^$', 'StockMarketPrediction.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^$', 'Predictions.views.index', name='index'),
-    url(r'^companies/$', 'Predictions.views.companies', name='companies'),
-    url(r'^selected_company/$', 'Predictions.views.selected_company', name='selected_company'),
-    url(r'^fundamental_analysis/(?P<company>[A-Z]+$)', 'Predictions.views.fundamental', name='fundamental'),
-    url(r'^technical_analysis/(?P<company>[A-Z]+$)', 'Predictions.views.technical', name='technical'),
-    url(r'^recommendation/$', 'Predictions.views.recommendation', name='recommendation'),
-    url(r'^update/$', 'Predictions.views.update', name='update'),
-    url(r'^accuracies/$', 'Predictions.views.accuracies', name='accuracies'),
+    re_path(r'^$', 'Predictions.views.index', name='index'),
+    re_path(r'^companies/$', 'Predictions.views.companies', name='companies'),
+    re_path(r'^selected_company/$', 'Predictions.views.selected_company', name='selected_company'),
+    re_path(r'^fundamental_analysis/(?P<company>[A-Z]+$)', 'Predictions.views.fundamental', name='fundamental'),
+    re_path(r'^technical_analysis/(?P<company>[A-Z]+$)', 'Predictions.views.technical', name='technical'),
+    re_path(r'^recommendation/$', 'Predictions.views.recommendation', name='recommendation'),
+    re_path(r'^update/$', 'Predictions.views.update', name='update'),
+    re_path(r'^accuracies/$', 'Predictions.views.accuracies', name='accuracies'),
 
 
 ]
